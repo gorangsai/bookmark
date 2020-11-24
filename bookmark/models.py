dd255c30f150b85eba8eb993abc79ff33f8e531f
unknown <kyosu>
2020-11-24 14:05:34 +0900

    Bookmark Service

---
+++ bookmark/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.urls import reverse
+
+class Bookmark(models.Model):
+    site_name = models.CharField('Title', max_length=100)
+    url = models.URLField('URL')
+
+    def __str__(self):
+        return "Name : "+self.site_name +", URL : " + self.url
+    def get_absolute_url(self):
+        return reverse('detail', args=[str(self.id)])
